a00201c87ed6c45ecba7e3327c980b120de1a0de
Juancho <linanjm90@gmail.com>
2020-05-20 10:13:25 -0500

    add some boostrap style to wrap the survey content

--- src/App.js
+++ src/App.js
@@ -36,8 +36,20 @@ class App extends Component {
 
 
         return (
-            <div className="App">
-              {survey}
+            <div className="container-fluid">
+              <div className="row justify-content-lg-center">
+                <div className="col col-lg-10">
+                  <div className="card border border-primary mt-5">
+                    <div className="card-body">
+                      <h5 className="card-title justify-content-center text-primary">Survey</h5>
+                      <h6 className="card-subtitle text-muted text-primary">Company Name</h6>
+                      <div className="App">
+                        {survey}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
         );
     }
