6b1e4a7139d20631e3a8d825efe300ad961d64c4
Juancho <linanjm90@gmail.com>
2020-05-22 12:29:02 -0500

    New component to wrap the survey component

---
+++ src/SurveyContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MySurvey from './MySurvey.js';
+
+class SurveyContainer extends Component {
+
+    contructor(props) {
+        super(pops);
+    }
+
+}
+
+export default SurveyContainer;
